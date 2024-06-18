fe6347681c0da1b5035210316b48cd75071db3bb
Roop Krrish <roopeshcena999@gmail.com>
2024-06-18 18:08:32 +0530

    dsfdsf

--- pages/Page1/jsobjects/JSObject1/JSObject1.js
+++ pages/Page1/jsobjects/JSObject1/JSObject1.js
@@ -4,7 +4,7 @@ export default {
 	myFun1 () {
 
 		const a =10;
-		console.log('helloopdfd');
+		console.log('helloopasfdsfdsfsdsfsdfdfd');
 	},
 	async myFun2 () {
 		//	use async-await or promises
